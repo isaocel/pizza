dc7b27f454427efa826dce534a90a98e420f6088
İsa Öcel <isaocel@gmail.com>
2025-02-27 14:30:12 +0300

    geri aldım

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -77,7 +77,7 @@ function Order() {
       form.ekMalzemeler.length <= 10 &&
       form.adet > 0;
 
-    isFormValid ? setIsValid(isFormValid) : console.log(errors);
+    setIsValid(isFormValid);
   }, [form]);
 
   function handleChange(event) {
